0becb9ccf9c09cc9f4bb6c7cc329cae2d377701b
Ariv <arivbits@gmail.com>
2018-07-15 00:37:32 +0530

    Added Rest Template

---
+++ src/main/java/com/example/demo/controller/PostResource.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package com.example.demo.controller;
+
+import java.io.IOException;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * @author zentere
+ *
+ */
+@RestController
+@RequestMapping("/api")
+public class PostResource {
+
+	private RestTemplate restTemplate;
+	
+	public PostResource(RestTemplateBuilder restTemplateBuilder) {
+		this.restTemplate = restTemplateBuilder.build();
+	}
+	
+	@GetMapping(value = "/posts")
+	public JsonNode listPosts() {
+		try {
+			ResponseEntity<String> response
+			  = restTemplate.getForEntity("https://oxygenna.com/wp-json/wp/v2/posts?_embed", String.class);
+			ObjectMapper mapper = new ObjectMapper();
+			JsonNode root = mapper.readTree(response.getBody());
+			return root;
+		} catch (RestClientException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
