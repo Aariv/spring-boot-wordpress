a478b79d61a55f1b292f7d7cd384d133bbe6bcec
Ariv <arivbits@gmail.com>
2018-07-20 18:37:46 +0530

    Added post request

--- src/main/java/com/example/demo/controller/PostAdminResource.java
+++ src/main/java/com/example/demo/controller/PostAdminResource.java
@@ -4,12 +4,11 @@
 package com.example.demo.controller;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,12 +16,8 @@ import com.afrozaar.wordpress.wpapi.v2.Wordpress;
 import com.afrozaar.wordpress.wpapi.v2.config.ClientConfig;
 import com.afrozaar.wordpress.wpapi.v2.config.ClientFactory;
 import com.afrozaar.wordpress.wpapi.v2.exception.PostCreateException;
-import com.afrozaar.wordpress.wpapi.v2.model.Content;
-import com.afrozaar.wordpress.wpapi.v2.model.Links;
 import com.afrozaar.wordpress.wpapi.v2.model.Post;
 import com.afrozaar.wordpress.wpapi.v2.model.PostStatus;
-import com.afrozaar.wordpress.wpapi.v2.model.Self;
-import com.afrozaar.wordpress.wpapi.v2.model.Title;
 import com.afrozaar.wordpress.wpapi.v2.response.PagedResponse;
 import com.example.demo.property.WordPressUtils;
 import com.fasterxml.jackson.databind.exc.MismatchedInputException;
@@ -71,30 +66,8 @@ public class PostAdminResource {
 		return null;
 	}
 
-	@PostMapping("/posts")
-	public Post addNewPost() throws MismatchedInputException {
-		Post post = new Post();
-		post.setAuthor(1L);
-		Content content = new Content();
-		content.setRendered("This is the content of the post");
-		post.setContent(content);
-		post.setStatus("publish");
-		Links links = new Links();
-		List<Self> self = new ArrayList<Self>();
-		links.setSelf(self);
-		post.setLinks(links);
-		post.setModified("");
-		Title title = new Title();
-		title.setRendered("New Title from dev");
-		post.setTitle(title);
-		
-		post.setType("post");
-		post.setPassword("uy");
-		post.setCommentStatus("closed");
-		post.setPingStatus("closed");
-		List<Long> categoryIds = new ArrayList<>();
-		post.setCategoryIds(categoryIds);
-		
+	@PostMapping(value = "/posts")
+	public Post addNewPost(@RequestBody Post post) throws MismatchedInputException {
 		try {
 			Post createdPost = client.createPost(post, PostStatus.draft);
 			return createdPost;
